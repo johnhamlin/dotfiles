39ef9b7c37bac45d7a829f661fc44d7750a85d96
John Hamlin <johnhamlin@gmail.com>
2025-11-26 21:18:56 -0500

    Update scheme, kitty support, expand lualine

--- lua/plugins/scheme.lua
+++ lua/plugins/scheme.lua
@@ -10,21 +10,49 @@ return {
   -- Rainbow brackets ONLY for Scheme/Racket
   {
     "HiPhish/rainbow-delimiters.nvim",
-    ft = { "scheme", "racket", "lisp", "clojure" }, -- lazy-load only for these
+    ft = { "scheme", "racket", "lisp", "clojure" },
     config = function()
       local rainbow = require("rainbow-delimiters")
       require("rainbow-delimiters.setup").setup({
         query = {
-          ['scheme'] = 'rainbow-delimiters',
-          ['racket'] = 'rainbow-delimiters',
+          ["scheme"] = "rainbow-delimiters",
+          ["racket"] = "rainbow-delimiters",
         },
       })
     end,
   },
 
-  -- REPL integration
+  -- REPL integration via Conjure
   {
     "Olical/conjure",
     ft = { "scheme", "racket" },
+    init = function()
+      vim.g["conjure#client#scheme#stdio#command"] = "chez"
+      vim.g["conjure#client#scheme#stdio#prompt_pattern"] = "> $?"
+    end,
+  },
+
+  -- Autopairs: disable ' pairing in lisps only
+  {
+    "windwp/nvim-autopairs",
+    -- extend LazyVim's existing config
+    config = function(_, opts)
+      local npairs = require("nvim-autopairs")
+      local Rule = require("nvim-autopairs.rule")
+      local cond = require("nvim-autopairs.conds")
+
+      -- keep LazyVim defaults
+      npairs.setup(opts)
+
+      -- remove the built-in single-quote rule
+      pcall(function()
+        npairs.remove_rule("'")
+      end)
+
+      -- re-add a single-quote rule, but NOT for lispy filetypes
+      npairs.add_rules({
+        Rule("'", "'"):with_pair(cond.not_filetypes({ "scheme", "racket", "lisp", "clojure" })),
+      })
+    end,
   },
 }
